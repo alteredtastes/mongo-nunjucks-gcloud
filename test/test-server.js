413e71215136081e1c8d1966d81e7cd1af9fea2e
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-08-28 00:18:10 -0700

    chai chai-http testing configured

--- test/test-server.js
+++ test/test-server.js
@@ -4,13 +4,17 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var mongoose = require('mongoose');
 
-var server = require('../app');
+var app = require('../app');
 var Artist = require('../db/models/moma/artist');
 
 var should = chai.should();
 chai.use(chaiHttp);
 
-describe('findArtists', function() {
+app.set('test', function(){
+  port = 3000;
+});
+
+describe('Artists', function() {
 
   Artist.collection.drop();
 
@@ -28,15 +32,17 @@ describe('findArtists', function() {
   });
 
   it('should list ALL artists on org/moma/artists GET', function(done) {
-    chai.request(server)
-      .get('org/moma/artists')
+    chai.request(app)
+      .get('/org/moma/artists')
       .end(function(err, res) {
+        console.log(app.settings.env);
+        if(err) console.log(err);
         res.should.have.status(200);
         done();
       });
   });
-  // it('should list a SINGLE artist on /artist/<id> GET');
-  // it('should add a SINGLE artist on /artists POST');
-  // it('should update a SINGLE artist on /artist/<id> PUT');
-  // it('should delete a SINGLE artist on /artist/<id> DELETE');
+  it('should list a SINGLE artist on /artist/<id> GET');
+  it('should add a SINGLE artist on /artists POST');
+  it('should update a SINGLE artist on /artist/<id> PUT');
+  it('should delete a SINGLE artist on /artist/<id> DELETE');
 });
